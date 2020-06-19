0e745c5efc7a199219ae6885f1bcefeb5a3de25a
GuldauGuldau <cweta84@gmail.com>
2020-06-19 16:39:34 +0300

    init app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    Welcome Tinetik
+    Welcome Tinetik co
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
